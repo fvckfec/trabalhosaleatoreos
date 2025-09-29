2901984131912a5be02c0163f374a77759b79e12
Fec <fernandosouza.carreira@gmail.com>
2025-09-29 20:01:40 -0300

    Add files via upload

--- rodape.php
+++ rodape.php
@@ -1,5 +1,5 @@
     <footer>
-        <p> &copy: 2024 - Todos os direitos reservados </p>
+        <p> &copy: 2025 - Todos os direitos reservados </p>
         <p> Desenvolvido por Fernando Souza Carreira </p>
     </footer>
 </body>
